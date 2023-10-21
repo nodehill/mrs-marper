6749f63d1ff5fe55265919c7fb1bbd9ce7060d33
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-10-21 17:20:21 +0200

    .

--- frontend/main.js
+++ frontend/main.js
@@ -1,25 +1,86 @@
 
+// Handle the form submission event, prevent default, and compose the request body
 
-// handle login form submission
+const [form] = document.forms;
+const [emailFeedback, passwordFeedback] =
+  document.querySelectorAll(".feedback");
 
-const loginForm = document.querySelector("loginForm");
-loginForm.addEventListener("submit", onFormSubmit);
-
-const onFormSubmit = async (event) => {
-  event.preventDefault();
-  const formData = new FormData(loginForm);
-  const data = Object.fromEntries(formData);
-  const response = await fetch("/api/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  const result = await response.json();
-  if (result.error) {
-    alert(result.error);
-  } else {
-    alert("You are now logged in!");
-  }
+const isEmailValid = (email) => {
+  return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g.test(
+    email
+  );
 };
+
+const isPasswordValid = (password) => {
+  return /^((?=.*[\d])(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[a-z])(?=.*[^\w\d\s])).{7,30}$/gm.test(
+    password
+  );
+};
+
+const validation = (email, password) => {
+  return isEmailValid(email) && isPasswordValid(password);
+};
+
+const toggleShowPassword = (toggler, elements) => {
+  toggler.addEventListener("change", (e) => {
+    elements.forEach((element) => {
+      element.setAttribute("type", e.target.checked ? "text" : "password");
+    });
+  });
+};
+
+const getElement = (name, e) => {
+  return {
+    email(e) {
+      e.target.classList.toggle("border-danger", !isEmailValid(e.target.value));
+      emailFeedback.textContent = isEmailValid(e.target.value)
+        ? null
+        : "Provide a valid email address";
+    },
+    password(e) {
+      e.target.classList.toggle(
+        "border-danger",
+        !isPasswordValid(e.target.value)
+      );
+      passwordFeedback.textContent = isPasswordValid(e.target.value)
+        ? null
+        : "Password must be at least 7 characters long and contain 1 capital letter and 1 symbol or number";
+    },
+  }[name](e);
+};
+
+const handleInput = (e) => {
+  const { email, password, btn } = form;
+  const { name } = e.target;
+
+  getElement(name, e);
+
+  btn.disabled = !validation(email.value, password.value);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  toggleShowPassword(form.showPassword, [form.password]);
+
+  form.email.addEventListener("input", handleInput);
+
+  form.password.addEventListener("input", handleInput);
+
+  form.addEventListener ("submit", async (e) => {
+    e.preventDefault();
+    const { email, password } = e.target;
+    const submittedValue = {
+      email: email.value,
+      password: password.value,
+    };
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(submittedValue),
+    });
+
+    console.log(submittedValue, JSON.s);
+  });
+});
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5173",
+        target: "http://localhost:5002",
         changeOrigin: true,
         secure: false,
       }
-    }
+    },
   }
 });
