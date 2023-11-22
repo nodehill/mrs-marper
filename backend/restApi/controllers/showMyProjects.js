9723a4a18222c00de4420bceb5445db649c4d544
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-11-22 10:36:28 +0100

    .

--- backend/restApi/controllers/showMyProjects.js
+++ backend/restApi/controllers/showMyProjects.js
@@ -24,13 +24,13 @@ export default ({ restRouter }) => {
       let files = readdirSync(projectFolder);
       return files;
     });
-
+    // TODO: Read the markdown file and send it to the frontend in a list
     let filename = pathJoin(myProjectFolder, projects[0], "index.md")
     let markdownContent = readFileSync(filename, "utf-8");
     let filename2 = pathJoin(myProjectFolder, projects[1], "index.md")
     let markdownContent2 = readFileSync(filename2, "utf-8");
     let filename3 = pathJoin(myProjectFolder, projects[2], "index.md")
-    let markdownContent3 = readFileSync(filename2, "utf-8");
+    let markdownContent3 = readFileSync(filename3, "utf-8");
 
     res.json({ innerFiles, projects, markdownContent, markdownContent2, markdownContent3 });
   });
